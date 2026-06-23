5e9f45597c3c186b05882e54af851991f2340e02
AdamB <adam@light-up.co.uk>
2026-06-23 09:52:36 +0100

    - merge error

--- src/mame/tektronix/tek440x.cpp
+++ src/mame/tektronix/tek440x.cpp
@@ -1542,6 +1542,8 @@ void tek440x_state::physical_map(address_map &map)
 	map(0x600000, 0x61ffff).ram().share("vram");
 
 	// 700000-71ffff spare 0
+	// 720000-720fff spare 1 (ethernet)
+	map(0x720000, 0x720007).rw(m_lance, FUNC(am79c90_device::regs_r), FUNC(am79c90_device::regs_w));
 	// 720000-73ffff spare 1
 
 	// maps 128 address range to nvram (see p2.8-3)
